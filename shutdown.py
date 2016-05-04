d4915b44c04b3647fa80fadc94368758f5bc9c8e
bluepython508 <you@gmail.com>
2016-05-04 15:47:58 +0100

    Fixed indent issues

--- shutdown.py
+++ shutdown.py
@@ -6,49 +6,48 @@ import threading
 shutdown = IO.Button(21, pull_up=False)
 indicator = IO.PWMLED(17)
 def call_shutdown():
-	for x in range(10):
-		indicator.value(1.0)
-		time.sleep(1)
-		indicator.value(0.0)
-		time.sleep(1)
-	call("sudo shutdown -h now", shell=True)
+        for x in range(10):
+                indicator.value(1.0)
+                time.sleep(1)
+                indicator.value(0.0)
+                time.sleep(1)
+        call("sudo shutdown -h now", shell=True)
 def frange(start, stop, step):
-  	i = start
- 
-  	if (start < stop):
-    	while i <= stop:
-	      	yield i
-    	  	i += step
-      # For some reason, += doesn't always add an exact decimal, so we have to round the value
-	      	i = round(i, 1)
-	 else:
-	    while i >= stop:
-	    	yield i
-    	  	i += step
-      	# For some reason, += doesn't always add an exact decimal, so we have to round the value
-     	 	i = round(i, 1)
+        i = start
+        if (start < stop):
+                while i <= stop:
+                        yield i
+                        i += step
+# For some reason, += doesn't always add an exact decimal, so we have to round the value
+                        i = round(i, 1)
+        else:
+                while i >= stop:
+                        yield i
+                        i += step
+# For some reason, += doesn't always add an exact decimal, so we have to round the value
+                        i = round(i, 1)
 shutdown.when_pressed = call_shutdown
 class RandomLEDs(threading.Thread):
-	def __init__(self, threadID, name):
-    	threading.Thread.__init__(self)
-    	self.threadID = threadID
-    	self.name = name
+        def __init__(self, threadID, name):
+                threading.Thread.__init__(self)
+                self.threadID = threadID
+                self.name = name
 
-	def run(self):
-    	while True:
-    		the_led = indicator
-    		self.fade_in_led(the_led, 0.03)
-    		time.sleep(0.3)
-    		self.fade_out_led(the_led, 0.02)
-    		time.sleep(0.3)
+        def run(self):
+                while True:
+                        the_led = indicator
+                        self.fade_in_led(the_led, 0.03)
+                        time.sleep(0.3)
+                        self.fade_out_led(the_led, 0.02)
+                        time.sleep(0.3)
 
-  # PWM the LED value from 0 to 1 (or from 1 to 0) with a 0.1 step
-	def fade_in_led(self, led, speed):
-    	for i in frange(0.0, 1.0, 0.1):
-      		led.value = i
-      		time.sleep(speed)
+# PWM the LED value from 0 to 1 (or from 1 to 0) with a 0.1 step
+        def fade_in_led(self, led, speed):
+                for i in frange(0.0, 1.0, 0.1):
+                        led.value = i
+                        time.sleep(speed)
 
-  	def fade_out_led(self, led, speed):
-    	for i in frange(1.0, 0.0, -0.1):
-      		led.value = i
-      		time.sleep(speed)
+        def fade_out_led(self, led, speed):
+                for i in frange(1.0, 0.0, -0.1):
+                        led.value = i
+                        time.sleep(speed)
