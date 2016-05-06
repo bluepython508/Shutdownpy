7ce2c84517cc82384aa1e9187d59c7ae4953cd46
Your Name <you@example.com>
2016-05-06 16:31:24 +0000

    added syslog feature

--- shutdown.py
+++ shutdown.py
@@ -2,13 +2,11 @@ import gpiozero as IO
 from signal import pause
 from subprocess import call
 import time
-import threading
 shutdown = IO.Button(21, pull_up=False)
 indicator = IO.PWMLED(17)
-shutdown_called = False
 def call_shutdown():
-        shutdown_called = True
         indicator.blink(on_time=1, off_time=1, n=10, background=False)
+        syslog.syslog("GPIO Interupt Detected. Shutting Down...")
         call("sudo shutdown -h now", shell=True)
 shutdown.when_pressed = call_shutdown
 indicator.blink(on_time=1, off_time=1, fade_in_time= 0.9, fade_out_time=0.9, n=None, background=False)
